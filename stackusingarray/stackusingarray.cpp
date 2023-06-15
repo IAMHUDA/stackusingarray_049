c2d3d734b6cfc95be225bd1c8eb78627fdda376d
IAMHUDA <hudamiftahul362@gmail.com>
2023-06-15 10:29:10 +0700

    membuat class program dengan dua acces private dan public

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -2,3 +2,17 @@
 #include <string>
 using namespace std;
  
+class program {
+private:
+	string stack_array[5];
+	int top;
+public:
+	program() {
+		top = -1;
+	}
+	string push(string element) {
+		if (top == 4) {
+			cout << "stack full" << endl;
+		}
+	}
+};
